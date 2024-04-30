3c36fc032dee17d71dd270df7f218d27330b518f
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-05-01 00:11:59 +0530

    Update Fibboacci.java

--- Fibboacci.java
+++ Fibboacci.java
@@ -3,7 +3,7 @@ public class Fibboacci {
         int a=0,b=1,c;
         System.out.print(a+"\t");
         System.out.print(b+"\t");
-     for(int i=1;i<5;i++)
+     for(int i=0;i<5;i++)
      {
          c=a+b;
          System.out.print(c+"\t");
