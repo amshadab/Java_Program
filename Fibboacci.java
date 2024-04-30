277494d40d7e0ee16fc39653a1ca9568ce7a6ee0
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-05-01 00:08:50 +0530

    Add files via upload

---
+++ Fibboacci.java
@@ -0,0 +1,14 @@
+public class Fibboacci {
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+        System.out.print(a+"\t");
+        System.out.print(b+"\t");
+     for(int i=1;i<5;i++)
+     {
+         c=a+b;
+         System.out.print(c+"\t");
+        a=b;
+        b=c;
+     }
+    }
+}
