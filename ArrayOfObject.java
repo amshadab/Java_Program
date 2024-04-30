559a50c3d7dd1995f34f77fc8dd7f80207ef9f6f
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-04-30 16:40:11 +0530

    Add files via upload

---
+++ ArrayOfObject.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Student
+{
+    int id=0;
+    String name;
+    public void SetData()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Student ID: ");
+        id = sc.nextInt();
+        System.out.print("Enter Student name: ");
+        name = sc.next();
+        
+    }
+    public void display()
+    {
+        System.out.println("Id: "+id);
+        System.out.println("Name: "+name);
+    }
+}
+public class ArrayOfObject {
+    public static void main(String[] args) {
+        Student[] obj = new Student[5];
+        for(int i=0;i<5;i++)
+        {
+            obj[i]=new Student();
+            obj[i].SetData();
+        }
+        for(int i=0;i<5;i++)
+        {
+            obj[i].display();
+        }
+        
+
+    }
+}
