4ab05203ddcfa8a5654b65641e3dc86fc99863f2
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-05-02 12:09:42 +0530

    Add files via upload

---
+++ Animal/Dog.java
@@ -0,0 +1,8 @@
+package Animal;
+
+public class Dog {
+    public void sound()
+    {
+        System.out.println("Dog bark");
+    }
+}
---
+++ PackageCode.java
@@ -0,0 +1,9 @@
+import Animal.Dog;
+
+public class PackageCode {
+
+    public static void main(String[] args) {
+        Dog obj = new Dog();
+        obj.sound();
+    }
+}
