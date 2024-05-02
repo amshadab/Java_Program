78ff7ee14349fe6174cd71698250858ad599821e
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-05-02 13:43:45 +0530

    Update Lines.java

--- Lines.java
+++ Lines.java
@@ -1,22 +1,20 @@
 import java.io.*;
-public class Words {
+public class Lines {
 
     public static void main(String[] args) throws IOException {
         File f = new File("Hello.txt");
         f.createNewFile();
         FileWriter fw = new FileWriter(f);
-        fw.write("Hello World in java");
+        fw.write("Hello World in java\n okay bye");
         fw.close();
         FileReader fr = new FileReader(f);
       BufferedReader br = new BufferedReader(fr);
-        String line;
         int count=0;
-        while((line=br.readLine())!=null)
+        while((br.readLine())!=null)
         {
-            String word[]=line.split(" ");
-            count = count + word.length;
+            count++;
         }
-        System.out.println("Number of words: "+count);
+        System.out.println("Number of lines: "+count);
         br.close();
         fr.close();
 
