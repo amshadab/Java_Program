7331553155ba591a7c251da3cff21b6f89f916d9
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2024-05-02 13:43:08 +0530

    Update Words.java

--- Words.java
+++ Words.java
@@ -4,20 +4,21 @@ public class Words {
     public static void main(String[] args) throws IOException {
         File f = new File("Hello.txt");
         f.createNewFile();
-        FileWriter fw = new FileWriter("Hello.txt");
+        FileWriter fw = new FileWriter(f);
         fw.write("Hello World in java");
         fw.close();
-        FileReader fr = new FileReader("Hello.txt");
-        int ch;
-        int word=0;
-        while((ch=fr.read())!=-1)
+        FileReader fr = new FileReader(f);
+      BufferedReader br = new BufferedReader(fr);
+        String line;
+        int count=0;
+        while((line=br.readLine())!=null)
         {
-            if(ch==' ')
-            {
-               word++;
-            }
+            String word[]=line.split(" ");
+            count = count + word.length;
         }
-        System.out.println("Number of words: "+ (word+1));
+        System.out.println("Number of words: "+count);
+        br.close();
         fr.close();
+
     }
 }
